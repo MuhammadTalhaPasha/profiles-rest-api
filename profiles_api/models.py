0eb5e71785cc139178801897068b69719d0a00aa
talhapasha <talhapasha81@gmail.com>
2019-11-24 16:40:26 +0500

    fixed the error and enable django admin for user profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,22 +7,22 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email ,fname ,lname ,password=None):
+    def create_user(self, email ,firstname ,lastname ,password=None):
         """create a new user profile"""
         if not email:
             raise ValueError('User must have a Email Address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, fname=fname, lname=lname,)
+        user = self.model(email=email, firstname=firstname, lastname=lastname,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email ,fname ,lname ,password):
+    def create_superuser(self, email ,firstname ,lastname ,password):
         """Create and save a new superuser with given details"""
-        user = self.create_user(email ,fname ,lname ,password)
+        user = self.create_user(email ,firstname ,lastname ,password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -34,24 +34,24 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
-    fname = models.CharField(max_length=255)
-    lname = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fname','lname']
+    REQUIRED_FIELDS = ['firstname','lastname']
 
     def get_full_name(self):
         """Retrieve full name for user"""
-        full_name = fname +' '+ lname
+        full_name = firstname +' '+ lastname
         return self.full_name
 
     def get_short_name(self):
         """Retrieve short name of user"""
-        return self.fname
+        return self.firstname
 
     def __str__(self):
         """Return string representation of user"""
